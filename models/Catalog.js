131e3409f08ca7af872bdac47ca72eed9ca9a240
Corey <cqyates@gmail.com>
2019-07-12 09:13:41 -0400

    added models

--- models/Catalog.js
+++ models/Catalog.js
@@ -1,1 +1,42 @@
-//this is for the mongoDB database model
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const CatalogSchema = new Schema({
+
+    patternNumber: {
+        type: Number,
+        required: true,
+        unique: false
+    },
+    company: {
+        type: String,
+        required: true,
+        unique: false
+    },
+    season:{
+        type: String,
+        required: false
+    },
+    month:{
+        type: String,
+        required: false
+    },
+    year:{
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    }
+
+})
+
+const Catalog = mongoose.model("Catalog", CatalogSchema);
+
+module.exports = Catalog;
