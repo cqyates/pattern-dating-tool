beb9337d6c763587cffe1c3f7a291a5b31c8454e
Corey <cqyates@gmail.com>
2019-07-19 17:42:58 -0400

    added DB controller

---
+++ controllers/DBcontroller.js
@@ -0,0 +1,40 @@
+//This file needs to be changed to match the catalog schema
+
+
+const db = require("../models");
+
+// Defining methods for the booksController
+module.exports = {
+  findAll: function(req, res) {
+    db.Catalog
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Catalog
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Catalog
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Catalog
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Catalog
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
