61ef8e33770c24bf4402877992bdb3ad5c2f99f7
Corey <cqyates@gmail.com>
2019-07-21 18:17:06 -0400

    added utils folder

--- models/Page.js
+++ models/Page.js
@@ -2,20 +2,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const pageSchema = new Schema({
-	number: { 
-        type: Number, 
-        required: true },
-	catalog: [{ 
-        type: Schema.Types.ObjectId,
-        ref: 'Catalog' }],
-	patterns: [{ 
-        type: Schema.Types.ObjectId, 
-        ref: 'Pattern' }],
-    imgURL: {
-        type: String,
-        required: true,
-        unique: true,
-    }
+        number: {
+                type: Number,
+                required: true
+        },
+        catalog: [{
+                type: Schema.Types.ObjectId,
+                ref: 'Catalog'
+        }],
+        patterns: [{
+                type: Schema.Types.ObjectId,
+                ref: 'Pattern'
+        }],
+        imgURL: {
+                type: String,
+                required: true,
+                unique: true,
+        }
 });
 
 const Page = mongoose.model("Page", pageSchema);
