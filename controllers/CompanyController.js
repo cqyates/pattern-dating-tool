c154534d8c82027cd56bd8e03b23460ff4684688
Corey <cqyates@gmail.com>
2019-07-24 15:25:01 -0400

    finished getCompanies route

--- controllers/CompanyController.js
+++ controllers/CompanyController.js
@@ -1,12 +1,14 @@
 //FIXME issue 1 since I have 4 models and 4 controllers is the require statement set up correctly?
 //FIXME issue 2 is the db.company set up correctly?  Do they need to match? Do they?
 //FIXME issue 3 what argument gets passed inside find to get the entire list of companies?
+//FIXME issue 4 change format of this page to name the function and export at the bottom.
+
 
 const db = require("../models/Company");
 
 module.exports = {
     findAll: function(req, res) {
-      db.Compnany
+      db.Company
       //What goes inside the find?
         .find()
         .then(dbModel => res.json(dbModel))
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { upload, uploadAWS } = require('../utils/image-upload');
+const companyController = require("../controllers/CompanyController")
 
 router.post("/api/imgupload", upload.single('file'), async (req, res) => {
   //const modelObject = {} 
@@ -16,6 +17,14 @@ router.post("/api/imgupload", upload.single('file'), async (req, res) => {
 router.get("/api", (req, res)=> {
   res.send("API is working")
 })
+
+//All aspects of this route should now be done.  It's job is to list the companies on the admin.js page.
+router.get('/api/companies', async (req, res) => {
+  const result = await companyController.findAll()
+  res.send({ result })
+})
+
+
 
 module.exports = router;
 
