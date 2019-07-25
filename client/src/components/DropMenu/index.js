2fc0c49475c4c339924ae465ea06d9f11fbfff77
Corey <cqyates@gmail.com>
2019-07-25 18:13:00 -0400

    fixed database issue with dustin

--- client/src/components/DropMenu/index.js
+++ client/src/components/DropMenu/index.js
@@ -1,48 +1,44 @@
 //FIXME this is not working.  It is suppose to bring in the company list from the MongoDB database.  It should load with companies when it component mounts
 
 import React, { Component } from "react";
-import API from "../../utils/API/api";
-// import dropdown from "react-bootstrap";
-
+import API from "../../utils/API";
+import { Dropdown } from "react-bootstrap"
 //this component will call the getCompanies api and populate a dropdown menu with the company names. Not currently working
 
 
 class DropMenu extends Component {
-    state = {
-       //this part is not right
-        // companies,
-        // isSelected
-    }
+  state = {
+    //this part is not right
+    companies: [],
+    isSelected: false
+  }
 
-    componentDidMount() {
-        this.getCompanies();
-    }
+  componentDidMount() {
+    this.getCompanies();
+  }
 
+  getCompanies() {
+    API.getCompanies()
+      .then(res => {
+        const companyList = res.data;
+        this.setState({ companies: companyList });
+      })
+      .catch(error => console.log(error));
+  }
 
-    // FIXME not sure about the query param.
-    getCompanies(query) {
-        API.getCompanies(query)
-          .then(res => {
-            //This console.log is not getting hit.
-            console.log(res)
-            const companyList = res.data.map(b => {
-              return {
-                companyList
-              };
-            });
-            this.setState({ companies: companyList });
-          })
-          .catch(error => console.log(error));
-      }
-
-// between these two divs is where the dropdown menu should go with the company list prop that is called from the mongoDB and mapped in the get companies function.
-      render() {
-        return (
-          <div>
-           
-          </div>
-        )
-      }
+  // between these two divs is where the dropdown menu should go with the company list prop that is called from the mongoDB and mapped in the get companies function.
+  render() {
+    return (
+      <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+  
+      </Dropdown.Toggle>
+    
+      <Dropdown.Menu>
+        <Dropdown.Item {...this.state.companies}></Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+    )
+  }
 }
-
-export default DropMenu;
+export default DropMenu
--- controllers/CompanyController.js
+++ controllers/CompanyController.js
@@ -1,17 +1,9 @@
-//FIXME issue 1 since I have 4 models and 4 controllers is the require statement set up correctly?
-//FIXME issue 2 is the db.company set up correctly?  Do they need to match? Do they?
-//FIXME issue 3 what argument gets passed inside find to get the entire list of companies?
+const db = require("../models");
 
-
-const db = require("../models/Company");
-
+// Defining methods for the booksController
 module.exports = {
-    findAll: function(req, res) {
-      db.companies
-        .find({})
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-    }
+  findAll: function() {
+   return db.Company.find({})
+      
+  }
 }
-
-//this gets called on the routes page as a get route.
