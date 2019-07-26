8cfa2ea1dd92e0554a5d7e4b6ca0f3ec4bdd7e86
whitney227 <whitneylcrawford@gmail.com>
2019-07-26 14:31:48 -0400

    shopping cart link opens in new tab

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from "react";
 import {Navbar, Nav} from "react-bootstrap";
 
@@ -9,13 +10,13 @@ import "./style.css";
         <Navbar bg="white" variant="light">
         {/* <Navbar.Brand className="navBrand" href="#home" style={{color:"#758696"}}>Vintage4Me2</Navbar.Brand> */}
         <Nav className="mr-auto">
-          <Nav.Link>
+          <div>
             <i className="fas fa-home" href="#home"></i>
-            <a href="https://www.vintage4me2.com/">
+            <a target="_blank" href="https://www.vintage4me2.com/">
               <i className="fas fa-shopping-cart"></i>
             </a>
             <i className="fas fa-lock"></i>
-          </Nav.Link>
+          </div>
         </Nav>
         <h1>Vintage 4 Me 2</h1>
         {/* <Form inline>
