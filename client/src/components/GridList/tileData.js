bfada9eaaf4a15d2c386cbe3ab9efe535fc55465
whitney227 <whitneylcrawford@gmail.com>
2019-07-27 14:12:52 -0400

    removed spaces in title

--- client/src/components/GridList/tileData.js
+++ client/src/components/GridList/tileData.js
@@ -2,68 +2,69 @@ const tileData = [
     {
       img: require('../../images/Butt-Summer-1934-Cover1.jpg'),
       title: 'cover',
-      author: 'Vintage4Me2',
+      author: 'Butterick Summer 1934',
       cols: 2,
       featured: true,
     },
     {
       img: require('../../images/Butt-Summer-1934-Cover2.jpg'),
       title: 'inside-cover',
-      author: 'Vintage4Me2',
+      author: 'Butterick Summer 1934',
     },
     {
       img: require('../../images/Butt-Summer-1934-page-1.jpg'),
       title: 'page1',
-      author: 'Vintage4Me2',
+      author: 'Butterick Summer 1934',
+      
     },
     {
       img: require('../../images/Butt-Summer-1934-page-2.jpg'),
       title: 'page2',
-      author: 'Vintage4Me2',
+      author: 'Butterick Summer 1934',
       featured: true,
     },
     {
       img: require('../../images/Butt-Summer-1934-page-3.jpg'),
       title: 'page3',
-      author: 'Vintage4Me2',
+      author: 'Butterick Summer 1934'
     },
     {
       img: require('../../images/Butt-Summer-1934-page-4.jpg'),
       title: 'page4',
-      author: 'Vintage4Me2',
+      author: 'Butterick Summer 1934',
     },
     {
       img: require('../../images/Butt-Summer-1934-page-5.jpg'),
       title: 'page5',
-      author: 'Vintage4Me2',
+      author: 'Butterick Summer 1934',
       cols: 2,
     },
     {
       img: require('../../images/Butt-Summer-1934-page-6.jpg'),
       title: 'page6',
-      author: 'Vintage4Me2',
+      author: 'Butterick Summer 1934'
     },
     {
         img: require('../../images/Butt-Summer-1934-page-7.jpg'),
         title: 'page7',
-        author: 'Vintage4Me2',
+        author: 'Butterick Summer 1934'
     },
     {
         img: require('../../images/Butt-Summer-1934-page-8.jpg'),
         title: 'page8',
-        author: 'Vintage4Me2',
+        author: 'Butterick Summer 1934'
     },
    
     {
       img: require('../../images/Butt-Summer-1934-page-9.jpg'),
       title: 'page9',
       cols: 2,
-      author: 'Vintage4Me2',
+      author: 'Butterick Summer 1934'
     },
     {
         img: require('../../images/Butt-Summer-1934-page-10.jpg'),
         title: 'page10',
-        author: 'Vintage4Me2',
+        author: 'Butterick Summer 1934'
       },
   ];
   
