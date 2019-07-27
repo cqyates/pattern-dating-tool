97ccd783111ca7f565e5b6dfc888028e1c8a341a
whitney227 <whitneylcrawford@gmail.com>
2019-07-27 13:57:07 -0400

    pattern search working

--- controllers/PatternController.js
+++ controllers/PatternController.js
@@ -3,10 +3,10 @@ const db = require("../models");
 // Defining methods for the booksController
 module.exports = {
    findByPatternNumber: function(query) {
+       console.log(query)
         return db.Pattern
        .find({patternNumber: query}) 
-       .populate("Page")
-       .then(dbModel => res.json(dbModel))
-       .catch(err => res.status(422).json(err));
+    //    .populate("Page")
+    
    }
 }
