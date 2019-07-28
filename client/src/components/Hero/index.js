1d44e14a67de27bbad56139a904eec1c204319d8
whitney227 <whitneylcrawford@gmail.com>
2019-07-28 12:48:05 -0400

    font change to title

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 
 function Hero() {
   return (
-    <div style={{boxSshadow: "10px 10px 5px grey"}}>
+    <div>
       <img className="heroNav text-center" src={hero} alt="hero" style={{width:"100%"}}/>
     </div>
   );
