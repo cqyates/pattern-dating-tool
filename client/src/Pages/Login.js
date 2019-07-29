6d4ee5c25d745c9926fd81a8e0f792a8b714189e
whitney227 <whitneylcrawford@gmail.com>
2019-07-29 15:30:28 -0400

    login form complete

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,10 +1,9 @@
 import React, {Component} from "react";
 import NavBar from "../components/NavBar";
 import Hero from "../components/Hero";
-import Wrapper from "../components/Wrapper";
-import LoginForm from "../components/Formik/LoginForm"
-import {Row, Col} from "react-bootstrap";
-
+import {Form, Card, Button} from "react-bootstrap";
+import Wrapper from "../components/Wrapper"
+import Footer from"../components/Footer"
 class Login extends Component {
     render() {
 
@@ -12,17 +11,29 @@ class Login extends Component {
             <div>
                 <NavBar />
                 <Hero />
-                <Wrapper />
-                <Row>
-                <Col size="sm-6" style={{margin:"0px 60px"}}>
-                    <LoginForm />
-                </Col>
-                </Row>
-                 
+                <Wrapper>
+                <Card style={{width:"60%", margin:"auto", padding:"20px"}}>
+                    <Form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Login
+                        </Button>
+                    </Form>             
+                </Card>
+                </Wrapper>
+                <Footer />
             </div>
+          
         )
     }
 }
-
-
-export default Login
+export default Login;
