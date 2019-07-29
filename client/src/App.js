b67a1e3d8631271033606c10174a2e594da9075d
whitney227 <whitneylcrawford@gmail.com>
2019-07-29 17:48:26 -0400

    password input not showing input

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,17 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Admin from "./Pages/Admin";
 import OCR from "./Pages/OCR";
 import Login from "./Pages/Login"
-import fire from "./config/fire"
 import "./App.css"
 
 
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-        user: {},
-    }
-  } 
+ function App() { 
 
-componentDidMount(){
-    this.authListener();
-}
-
-authListener() {
-    fire.auth().onAuthStateChanged((user) => {
-        if (user) {
-            this.setState({ user });
-        } else {
-            this.setState({ user: null });
-        }
-    });
-}
-  render() {
     return (
       <div className="App">
-        {/* {this.state.user ? (<Admin />) : (<Home/>)} */}
-
+    
         <Router>
             <Route exact path="/" component={Home} />
             <Route exact path="/home" component={Home} />
@@ -43,7 +21,6 @@ authListener() {
         </Router>
       </div>
     );
-  }
 }
 
 export default App;
--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,10 +1,12 @@
 import React, {Component} from "react";
 import NavBar from "../components/NavBar";
 import Hero from "../components/Hero";
-import {Form, Card, Button} from "react-bootstrap";
+import {Form, Card, Button, Row, Col} from "react-bootstrap";
 import Wrapper from "../components/Wrapper"
 import Footer from"../components/Footer";
-import fire from "../config/fire"
+import fire from "../config/fire";
+import Admin from "../Pages/Admin";
+import Home from "../Pages/Home";
 
 
 
@@ -15,9 +17,22 @@ class Login extends Component {
         this.handleChange = this.handleChange.bind(this);
         this.state = {
             email:"",
-            password:""
+            password:"",
+            user: {},
         }
     }
+    componentDidMount(){
+        this.authListener();
+    }
+    authListener() {
+      fire.auth().onAuthStateChanged((user) => {
+        if (user) {
+            this.setState({ user });
+          } else {
+            this.setState({ user: null });
+          }
+        });
+      }
     login(e) {
         e.preventDefault();
         fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u) => {
@@ -31,13 +46,15 @@ class Login extends Component {
         render()  {
         return(
             <div>
+                {/* {this.state.user ? <Admin /> : <Home/>} */}
                 <NavBar />
                 <Hero />
                 <Wrapper>
                 <Card style={{width:"60%", margin:"auto", padding:"20px"}}>
-                    <Form>
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
+                    <Form >
+                        <Form.Group as={Row} controlId="formBasicEmail">
+                            <Form.Label column sm="2">Email address</Form.Label>
+                            <Col sm="10">
                             <Form.Control 
                             type="email" 
                             placeholder="Enter email"
@@ -45,22 +62,26 @@ class Login extends Component {
                             onChange={this.handleChange}
                             name="email"
                             />
+                            </Col>
                         </Form.Group>
 
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
+                        <Form.Group as={Row} controlId="formPlaintextPassword">
+                            <Form.Label column sm="2">Password</Form.Label>
+                            <Col sm="10">
                             <Form.Control 
-                            type="password" 
+                            required
+                            type="text" 
                             placeholder="Password" 
                             value={this.state.password}
                             onChange={this.handleChange}
                             />
+                            </Col>
                         </Form.Group>
 
-                        <Button variant="primary" type="submit" onClick={this.login}>
-                            Login
-                        </Button>
                     </Form>             
+                    <Button style={{margin:"auto"}}variant="primary" type="submit" onClick={this.login}>
+                        Login
+                    </Button>
                 </Card>
                 </Wrapper>
                 <Footer />
