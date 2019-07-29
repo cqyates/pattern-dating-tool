3ba4bf38870632ec9a36055234817d22db3195b7
Corey <cqyates@gmail.com>
2019-07-29 12:33:23 -0400

    after tutoring

--- controllers/PageController.js
+++ controllers/PageController.js
@@ -3,18 +3,18 @@ const db = require("../models");
 
 
 module.exports = {
-    postPage: function(req) {
-       console.log(req.body)
-       return db.Page
-       .create({patternNumberArray: req.body.patternNumberArray, imgURL: filePath}) 
-       .populate("catalog")
-       //From this point how does the information flow through my different collections. 
-       //Page has a reference to catalog which holds the details company (another reference), season and year
+   postPage: function (req) {
+      console.log(req.body)
+      return db.Page
+         .create({ patternNumberArray: req.body.patternNumberArray, imgURL: filePath })
+         .populate("catalog")
+      //From this point how does the information flow through my different collections. 
+      //Page has a reference to catalog which holds the details company (another reference), season and year
 
-       //I also need to use a for loop to get through the pattern array and create an entry for each pattern number.
+      //I also need to use a for loop to get through the pattern array and create an entry for each pattern number.
 
-     
-    },
 
-  
- 
+   },
+
+
+}
