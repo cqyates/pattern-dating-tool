80c12240b9743b87edc63ff98611f02693c2916d
whitney227 <whitneylcrawford@gmail.com>
2019-07-30 23:13:15 -0400

    login route needed on app page

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Admin from "./Pages/Admin";
 import OCR from "./Pages/OCR";
-// import Login from "./Pages/Login"
+import Login from "./Pages/Login"
 import "./App.css"
 
 function App () { 
@@ -14,7 +14,7 @@ function App () {
             <Route exact path="/home" component={Home} />
             <Route exact path="/admin" component={Admin} />
             <Route exact path="/OCR" component={OCR} />
-            {/* <Route exact path="/login" component={Login} /> */}
+            <Route exact path="/login" component={Login} /> 
         </Router>
       </div>
     );
