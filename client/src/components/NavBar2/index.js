0d6272076845d615b93caeedcfcf7e7a5d9e1936
whitney227 <whitneylcrawford@gmail.com>
2019-07-31 12:16:56 -0400

    logout redirects to home

--- client/src/components/NavBar2/index.js
+++ client/src/components/NavBar2/index.js
@@ -5,29 +5,20 @@ import "./style.css";
 import fire from "../../config/fire"
  
 class NavBar2 extends Component {
-  constructor(props) {
-    super(props);
-    this.logout = this.logout.bind(this);
-  }
-  
-  logout(){
-    fire.auth().signOut();
-  }
+ 
     render() {
       return (
         <div className="navBar">
         <Navbar className="NavBar" bg="white" variant="light">
         <Nav className="mr-auto">
           <div>
-          <Button onclick={this.logout}>Logout</Button>
+          <Button>
+            <a href="/">Logout</a>
+          </Button>
           </div>
         </Nav>
         <h1>Vintage4Me2</h1>
-        {/* <Form inline>
-          <Button variant="outline-primary">
-            Login
-          </Button>
-        </Form> */}
+      
       </Navbar>
       </div>
     )
