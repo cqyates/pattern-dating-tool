5525770c4c6c976fbc7c8627f3543ce3aaa941dd
Corey <cqyates@gmail.com>
2019-08-01 18:36:37 -0400

    added tweaks

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 59077;
 
 // Define middleware here
 app.use(cors());
